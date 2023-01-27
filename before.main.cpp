6469b39b8e173427194c2c33a5bb965c3278c077
ArturArakelyan1 <123729449+ArturArakelyan1@users.noreply.github.com>
2023-01-27 14:48:51 +0400

    Add files via upload

---
+++ before.main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  main.cpp
+//
+//  Created by Artur Arakelyan on 27.01.23.
+//
+
+#include <iostream>
+using namespace std;
+ struct Hello
+{
+    Hello()
+    {
+        cout << "Hello world \n" << endl;
+    }
+} obj;
+ 
+int main()
+{
+    cout << "Hello world in the main \n";
+}
